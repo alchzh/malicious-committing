0d4268353ebf2f9bba8eb1610c5439047ec17ca0
Albert Zhang <alchzh@gmail.com>
2021-01-27 04:41:01 +0000

    Added characters '$'OUTFILE)"\n''

--- malcommit.sh
+++ malcommit.sh
@@ -50,4 +50,4 @@ while IFS="" read -r $READFLAGS c || [ "$c" ]; do
         CMTMSG=$(printf "Added characters '%q'" "$c")
     fi
 
-    git -C "$DIR" add "$(basename $
+    git -C "$DIR" add "$(basename $OUTFILE)"
