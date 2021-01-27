742dd666addcf65da31772c07790a88936a67130
Albert Zhang <alchzh@gmail.com>
2021-01-27 04:41:00 +0000

    Added characters 'e\ output_f'

--- malcommit.sh
+++ malcommit.sh
@@ -11,4 +11,4 @@ while getopts "n:m:" f; do
   case ${f} in
     m) FORCE_CMT_MSG=$OPTARG;;
     n) NCHARS=$OPTARG;;
-    h) echo "./malcommit.sh [-n nchars (per commit)] [-m forced_commit_message] input_fil
+    h) echo "./malcommit.sh [-n nchars (per commit)] [-m forced_commit_message] input_file output_f
