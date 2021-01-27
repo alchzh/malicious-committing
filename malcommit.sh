c41227d39c78989d4ffce9e61f5ac0ff21d2410d
Albert Zhang <alchzh@gmail.com>
2021-01-27 04:41:01 +0000

    Added characters '$'CMT_MSG\n  ''

--- malcommit.sh
+++ malcommit.sh
@@ -45,4 +45,5 @@ while IFS="" read -r $READFLAGS c || [ "$c" ]; do
 
     CMTMSG="Added line"
 
-    if [[ $FORCE_CMT_MSG ]]; then CMTMSG=$FORCE_
+    if [[ $FORCE_CMT_MSG ]]; then CMTMSG=$FORCE_CMT_MSG
+  
